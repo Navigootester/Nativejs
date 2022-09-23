b2cafe682faca2f44b872354b750297d4bddb9e5
Navigoo <81353827+Navigoo@users.noreply.github.com>
2022-09-23 21:50:36 +0200

    Commit

---
+++ index.js
@@ -0,0 +1,73 @@
+/*Välkommen till Native Javascript Kursen 2022!
+I denna fil kommer all JS kod att publiceras allt efterssom vi går igenom varje 
+sektion på lektionerna.
+Kommentarer och termer kommer att genomföras på engelska för att ni lättare sen kan 
+arbeta som i branschen.
+De kod-projekt som kommer göras under kursens gång läggs på annan sida.
+Övertygad om att ni kommer klara det här jättebra och önskar alla lycka till!!*/
+
+
+/* Konsolen, script-elementet och kommentarer */
+/* 1. Konsollen 2. Script elementet 3. Kommentarer */
+/*
+1. Konsollen öppnar man genom att antingen:
+- Högerklicka och trycka på inspektera, därefter trycka på konsol
+eller
+- På Mac opt+cmd+j
+- På PC ctrl+shift+k (eller ctrl+shift+j)
+
+Kod kan skrivas direkt i konsollen, exempel:
+alert("hej");
+Eller logga i konsollen direkt, exempel:
+console.log("hej");
+
+2. Scriptelementet lägger ni direkt på er html sida (innan body taggen slutar eller inom head elementet längst upp): <script src"länk-till-er-fil"><script> 
+
+3. Kommenterar gör man med //= för en rad eller som i css 
+
+*/
+
+
+/* Variabler, Konstanter och datatyper */
+/* 1. Variables, 2. Contants and 3. data types */
+
+/* 1. Variables */
+
+
+
+/* 2. Constants */
+
+
+
+/* 3. Data Types */
+
+
+
+/* Operatorer för aritmetik (och strängsammanslagning) */
+/* Operators and String Concats */
+
+/* 1. Operators */
+
+
+
+/* 2. String Con */
+
+
+
+
+/* 3. Data Types */
+
+
+
+
+/* Logic Operators */
+
+
+
+
+/* if och else, samt switch */
+
+
+
+
+/* while, do...while och for */
